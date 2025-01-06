6e7c7289e32388a665ad35d744702ff329df68f0
A. M. Arka <amarka310@gmail.com>
2025-01-06 23:29:33 +0600

    User api fetch 2.0

--- vital-aid-frontend/src/components/UserProfile/ChagePassword.js
+++ vital-aid-frontend/src/components/UserProfile/ChagePassword.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useGlobalContext } from '../../context/GlobalContext'
 import { useNavigate } from 'react-router-dom'
 import '../../style/UserProfile/ChangePassword.scss'
